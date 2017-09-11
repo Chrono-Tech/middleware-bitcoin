57b5ae7c71f30bdb090974f268722077d8efc70f
zyevegor <zyev.egor@gmail.com>
2017-09-11 10:39:52 +0300

    https://chronobank.atlassian.net/browse/MINT-446 - Middleware (Bitcoin) - integration tests

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,8 +8,8 @@ module.exports = {
       name: 'block_processor',
       script: 'core/blockProcessor',
       env: {
-        MONGO_URI: 'mongodb://localhost:32772/data',
-        RABBIT_URI: 'amqp://localhost:32769',
+        MONGO_URI: 'mongodb://localhost:27017/data',
+        RABBIT_URI: 'amqp://localhost:5672',
         BITCOIN_NETWORK: 'regtest',
         BITCOIN_DB: 'memory',
         BITCOIN_IPC: 'bitcoin',
@@ -21,8 +21,8 @@ module.exports = {
       name: 'balance_processor',
       script: 'core/balanceProcessor',
       env: {
-        MONGO_URI: 'mongodb://localhost:32772/data',
-        RABBIT_URI: 'amqp://localhost:32769',
+        MONGO_URI: 'mongodb://localhost:27017/data',
+        RABBIT_URI: 'amqp://localhost:5672',
         BITCOIN_IPC: 'bitcoin',
         BITCOIN_IPC_PATH: '/tmp/',
       }
@@ -31,7 +31,7 @@ module.exports = {
       name: 'rest',
       script: 'core/rest',
       env: {
-        MONGO_URI: 'mongodb://localhost:32772/data',
+        MONGO_URI: 'mongodb://localhost:27017/data',
         REST_PORT: 8082,
         BITCOIN_IPC: 'bitcoin',
         BITCOIN_IPC_PATH: '/tmp/'
